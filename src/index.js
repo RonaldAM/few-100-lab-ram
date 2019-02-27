b06d1cf778ad0ae70af6277148fde6430f3b80f3
ron <wholesomevalues@gmail.com>
2019-02-27 08:14:40 -0500

    so far so good

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import './styles.css';
+import { ready } from './utils';
+const billAmount = document.getElementById('billAmount');
+const text = document.getElementById('lblText');
+const tipPercentage = document.getElementById('tipPerentage');
+const tipAmount = document.getElementById('tipAmount');
+const amountDue = document.getElementById('amountDue');
+[...document.querySelectorAll('.btnTipClass')]
+    .forEach(function (item) {
+        item.addEventListener('click', function () {
+            enableAllTipButtons();
+            this.setAttribute("disabled", "disabled");
+            tipPercentage.innerHTML = this.innerHTML.substring(0, 3);
+            lblText.innerHTML = "You are tipping ... " + this.innerHTML;
+
+            let tip = parseFloat(tipPerentage.innerHTML) / 100;
+            tipAmount.innerHTML = (billAmount.value * tip).toFixed(2);
+            amountDue.innerHTML = (Number(tipAmount.innerHTML) + Number(billAmount.value)).toFixed(2);
+            console.log(amountDue);
+        })
+    });
+
+function enableAllTipButtons() {
+    [...document.querySelectorAll('.btnTipClass')]
+        .forEach(function (item) {
+            item.disabled = false;
+        });
+};
+
+console.log('Ready to Party');
+ready(() => { alert('i am ready') });
