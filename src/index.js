f0975d4a31da0709da627a0636937e16de387968
ron <wholesomevalues@gmail.com>
2019-02-27 12:22:28 -0500

    done for today

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './styles.css';
 import { ready } from './utils';
 const billAmount = document.getElementById('billAmount');
-const text = document.getElementById('lblText');
 const tipPercentage = document.getElementById('tipPerentage');
 const tipAmount = document.getElementById('tipAmount');
 const amountDue = document.getElementById('amountDue');
@@ -9,9 +8,11 @@ const amountDue = document.getElementById('amountDue');
 billAmount.addEventListener("focusout", function () {
     if (isNaN(this.value)) {
         this.classList.add("is-invalid");
+        clearAmounts();
     }
     else {
         this.classList.remove("is-invalid");
+        calculateAmounts();
     }
 });
 
@@ -22,12 +23,23 @@ billAmount.addEventListener("focusout", function () {
             this.setAttribute("disabled", "disabled");
             tipPercentage.innerHTML = this.innerHTML.substring(0, 3);
             lblText.innerHTML = "You are tipping ... " + this.innerHTML;
-
-            let tip = parseFloat(tipPerentage.innerHTML) / 100;
-            tipAmount.innerHTML = (billAmount.value * tip).toFixed(2);
-            amountDue.innerHTML = (Number(tipAmount.innerHTML) + Number(billAmount.value)).toFixed(2);
+            if (isNaN(billAmount.value))
+                clearAmounts();
+            else
+                calculateAmounts();
         })
     });
+
+function clearAmounts() {
+    tipAmount.innerHTML = "";
+    amountDue.innerHTML = "";
+}
+
+function calculateAmounts() {
+    let tip = parseFloat(tipPerentage.innerHTML) / 100;
+    tipAmount.innerHTML = (billAmount.value * tip).toFixed(2);
+    amountDue.innerHTML = (Number(tipAmount.innerHTML) + Number(billAmount.value)).toFixed(2);
+}
 
 function enableAllTipButtons() {
     [...document.querySelectorAll('.btnTipClass')]
